e1356b42ca1da494c333f7a2306d5de4cfab41c2
Ade Arwans <smkintel@gmail.com>
2014-12-11 11:54:28 +0700

    Update logout.php
    Sesuaikan path "http://localhost/sipaderw2/auth"

--- sipaderw2/inc/logout.php
+++ sipaderw2/inc/logout.php
@@ -4,5 +4,5 @@ session_destroy();
 ?>
 <script src="../rakstrap/jquery-2.1.1.min.js"></script>
 <script type="text/javascript">  
-top.window.location.href='http://localhost/sipaderw2blank/auth';
+top.window.location.href='http://localhost/sipaderw2/auth';
 </script>
